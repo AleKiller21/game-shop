1e6d6fc442fcb0f24489f18b1396109955819eaf
AleKiller21 <aleferrera@hotmail.com>
2018-04-09 15:25:38 -0600

    Create repo. Define login view

---
+++ src/components/Login/Login.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+// import '../../assets/css/login.css'
+
+class Login extends Component {
+    render () {
+        return(
+            <div className="limiter">
+		<div className="container-login100">
+			<div className="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
+				<form className="login100-form validate-form">
+					<span className="login100-form-title p-b-33">
+						Account Login
+					</span>
+
+					<div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input className="input100" type="text" name="email" placeholder="Email" />
+						<span className="focus-input100-1"></span>
+						<span className="focus-input100-2"></span>
+					</div>
+
+					<div className="wrap-input100 rs1 validate-input" data-validate="Password is required">
+						<input className="input100" type="password" name="pass" placeholder="Password" />
+						<span className="focus-input100-1"></span>
+						<span className="focus-input100-2"></span>
+					</div>
+
+					<div className="container-login100-form-btn m-t-20">
+						<button className="login100-form-btn">
+							Sign in
+						</button>
+					</div>
+
+					<div className="text-center p-t-45 p-b-4">
+						<span className="txt1">
+							Forgot
+						</span>
+
+						<a href="#" className="txt2 hov1">
+							Username / Password?
+						</a>
+					</div>
+
+					<div className="text-center">
+						<span className="txt1">
+							Create an account?
+						</span>
+
+						<a href="#" className="txt2 hov1">
+							Sign up
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+        );
+    }
+}
+
+export default Login;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './components/Login/Login'
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Login />, document.getElementById('root'));
+registerServiceWorker();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Button from 'material-ui/Button';
+
+// function App() {
+//   return (
+//     <Button variant="raised" color="primary">
+//       Hello World
+//     </Button>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
