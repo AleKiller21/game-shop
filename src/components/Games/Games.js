18e6067631b01cf4bc0c570c33363ca3bda49bfb
AleKiller21 <aleferrera@hotmail.com>
2018-04-11 13:56:10 -0600

    Add a little template to show a grid of games

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -3,13 +3,29 @@ import React, { Component } from 'react';
 import stateService from '../../services/stateService';
 
 class Games extends Component {
-    componentDidMount () {
+    componentDidMount() {
         stateService.getFunction('changeNavTitle')('Games');
     }
 
-    render () {
+    render() {
         return (
-            <p>Hello World</p>
+            <div className="row text-center text-lg-left">
+                <div className="col-lg-3 col-md-4 col-xs-6">
+                    <a href="#" className="d-block mb-4 h-100">
+                        <img className="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="" />
+                    </a>
+                </div>
+                <div className="col-lg-3 col-md-4 col-xs-6">
+                    <a href="#" className="d-block mb-4 h-100">
+                        <img className="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="" />
+                    </a>
+                </div>
+                <div className="col-lg-3 col-md-4 col-xs-6">
+                    <a href="#" className="d-block mb-4 h-100">
+                        <img className="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="" />
+                    </a>
+                </div>
+            </div>
         );
     }
 }
