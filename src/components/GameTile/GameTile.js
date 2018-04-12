4ff91f89ec16e5a08a3db45f12ae80da0ff4b933
AleKiller21 <aleferrera@hotmail.com>
2018-04-11 18:28:02 -0600

    Add GameTile component

---
+++ src/components/GameTile/GameTile.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class GameTile extends Component {
+    constructor (props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="col-lg-3 col-md-4 col-xs-6">
+                <Link to={`/game/${this.props.title}`} className="d-block mb-4 h-100">
+                    <img className="img-fluid img-thumbnail" src={this.props.url} alt={this.props.title} />
+                </Link>
+            </div>
+        );
+    }
+}
+
+GameTile.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+};
+
+export default GameTile;
--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import Button from 'material-ui/Button';
 import AddIcon from '@material-ui/icons/Add';
 
+import GameTile from '../GameTile/GameTile';
+
 import stateService from '../../services/stateService';
 import authService from '../../services/authService';
+import apiService from '../../services/apiService';
 
 import '../../assets/css/games.css';
 
@@ -11,15 +14,23 @@ class Games extends Component {
     constructor (props) {
         super(props);
 
-        this.state = { addBtnClasses: 'add-game-btn hide-add-game-btn' };
+        this.state = { addBtnClasses: 'add-game-btn hide-add-game-btn', games: [] };
     }
 
     async componentDidMount() {
         stateService.getFunction('changeNavTitle')('Games');
         const isAdmin = await authService.isCurrentUserAdmin();
         const addBtnClasses = isAdmin ? 'add-game-btn' : 'add-game-btn hide-add-game-btn';
+        let games = [];
 
-        this.setState({ addBtnClasses });
+        try {
+            games = await apiService.sendRequest('/games', 'GET');
+        } catch (err) {
+            console.log(err);
+            stateService.getFunction('showNotification')('error', 'Error', 'The game list was not able to load correctly');
+        }
+
+        this.setState({ addBtnClasses, games });
     }
 
     goToGameForm () {
@@ -33,26 +44,7 @@ class Games extends Component {
                     <AddIcon />
                 </Button>
                 <div className="row text-center text-lg-left">
-                    <div className="col-lg-3 col-md-4 col-xs-6">
-                        <a href="#" className="d-block mb-4 h-100">
-                            <img className="img-fluid img-thumbnail" src="http://www.mobygames.com/images/covers/l/38333-halo-2-xbox-front-cover.jpg" alt="" />
-                        </a>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-xs-6">
-                        <a href="#" className="d-block mb-4 h-100">
-                            <img className="img-fluid img-thumbnail" src="http://www.mobygames.com/images/covers/l/47712-star-wars-battlefront-xbox-front-cover.jpg" alt="" />
-                        </a>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-xs-6">
-                        <a href="#" className="d-block mb-4 h-100">
-                            <img className="img-fluid img-thumbnail" src="http://www.mobygames.com/images/covers/l/247564-gears-of-war-3-xbox-360-front-cover.jpg" alt="" />
-                        </a>
-                    </div>
-                    <div className="col-lg-3 col-md-4 col-xs-6">
-                        <a href="#" className="d-block mb-4 h-100">
-                            <img className="img-fluid img-thumbnail" src="http://www.mobygames.com/images/covers/l/207217-fable-iii-xbox-360-front-cover.jpg" alt="" />
-                        </a>
-                    </div>
+                    { this.state.games.map(game => <GameTile title={game.name} url={game.image} key={game.id} />) }
                 </div>
             </div>
         );
