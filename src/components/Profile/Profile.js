6371a4b34b4e523a81e45b65e735656c489a4a0b
AleKiller21 <aleferrera@hotmail.com>
2018-04-13 08:34:55 -0600

    Add logic to update  profile info

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,82 @@
 import React, { Component } from 'react';
 
+import apiService from '../../services/apiService';
+import stateService from '../../services/stateService';
+
 class Profile extends Component {
+    constructor () {
+        super();
+
+        this.state = {
+            id: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+            address: '',
+            gamertag: '',
+            profilePicture: ''
+        };
+    }
+
+    async componentDidMount () {
+        // try {
+        //     const gamertag
+        //     const response = apiService.sendRequest(`/user/`)
+        // } catch (err) {
+        //     console.log(err);
+        //     stateService.getFunction('showNotification')('error', 'Profile Failure', err.response.data.message);
+        // }
+
+        const { address, email, first_name, gamertag, id, last_name, profile_picture } = await stateService.getData('userInfo');
+
+        stateService.getFunction('changeNavTitle')(gamertag);
+        this.setState({ id, email, gamertag, address, firstName: first_name, lastName: last_name, profilePicture: profile_picture });
+    }
+
+    async updateProfile (e) {
+        e.preventDefault();
+
+        const { id, firstName, lastName, email, address, gamertag, profilePicture } = this.state;
+        const body = { id, address, email, gamertag, first_name: firstName, last_name: lastName, profile_picture: profilePicture };
+
+        try {
+            await apiService.sendRequest('/user/update', 'POST', body);
+            stateService.getFunction('showNotification')('success', 'Profile Updated', 'Your profile has been updated');
+            stateService.addData('userInfo', body);
+        } catch (err) {
+            console.log(err.response.data.message);
+            stateService.getFunction('showNotification')('error', 'Update failed', err.response.data.message);
+        }
+    }
+
+    changeGamertag (e) {
+        const gamertag = e.target.value;
+        this.setState({ gamertag });
+    }
+
+    changeFirstName (e) {
+        const firstName = e.target.value;
+        this.setState({ firstName });
+    }
+
+    changeLastName (e) {
+        const lastName = e.target.value;
+        this.setState({ lastName });
+    }
+
+    changeEmail (e) {
+        const email = e.target.value;
+        this.setState({ email });
+    }
+
+    changeAddress (e) {
+        const address = e.target.value;
+        this.setState({ address });
+    }
+
     render () {
+        const { firstName, lastName, email, address, gamertag, profilePicture } = this.state;
+
         return (
             <div className="row">
                         <div className="col-md-8">
@@ -11,24 +86,24 @@ class Profile extends Component {
                                     <p className="card-category">Complete your profile</p>
                                 </div>
                                 <div className="card-body">
-                                    <form>
+                                    <form onSubmit={e => this.updateProfile(e)}>
                                         <div className="row">
                                             <div className="col-md-5">
                                                 <div className="form-group">
                                                     <label className="bmd-label-floating">Gamertag</label>
-                                                    <input type="text" className="form-control" />
+                                                    <input type="text" value={gamertag} className="form-control" onChange={(e) => this.changeGamertag(e)} />
                                                 </div>
                                             </div>
                                             <div className="col-md-3">
                                                 <div className="form-group">
                                                     <label className="bmd-label-floating">First Name</label>
-                                                    <input type="text" className="form-control" />
+                                                    <input type="text" value={firstName} className="form-control" onChange={(e) => this.changeFirstName(e)} />
                                                 </div>
                                             </div>
                                             <div className="col-md-4">
                                                 <div className="form-group">
                                                     <label className="bmd-label-floating">Last Name</label>
-                                                    <input type="email" className="form-control" />
+                                                    <input type="text" value={lastName} className="form-control" onChange={(e) => this.changeLastName(e)} />
                                                 </div>
                                             </div>
                                         </div>
@@ -36,21 +111,13 @@ class Profile extends Component {
                                             <div className="col-md-6">
                                                 <div className="form-group">
                                                     <label className="bmd-label-floating">Email</label>
-                                                    <input type="text" className="form-control" />
+                                                    <input type="email" value={email} className="form-control" onChange={(e) => this.changeEmail(e)} />
                                                 </div>
                                             </div>
                                             <div className="col-md-6">
                                                 <div className="form-group">
                                                     <label className="bmd-label-floating">Address</label>
-                                                    <input type="text" className="form-control" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-12">
-                                                <div className="form-group">
-                                                    <label className="bmd-label-floating">Adress</label>
-                                                    <input type="text" className="form-control" />
+                                                    <input type="text" value={address} className="form-control" onChange={(e) => this.changeAddress(e)} />
                                                 </div>
                                             </div>
                                         </div>
@@ -64,12 +131,12 @@ class Profile extends Component {
                             <div className="card card-profile">
                                 <div className="card-avatar">
                                     <a href="#pablo">
-                                        <img className="img" src="../assets/img/faces/marc.jpg" />
+                                        <img className="img" src={profilePicture} />
                                     </a>
                                 </div>
                                 <div className="card-body">
-                                    <h6 className="card-category text-gray">CEO / Co-Founder</h6>
-                                    <h4 className="card-title">Alec Thompson</h4>
+                                    <h6 className="card-category text-gray">{firstName}</h6>
+                                    <h4 className="card-title">{gamertag}</h4>
                                     <p className="card-description" />
                                 </div>
                             </div>
